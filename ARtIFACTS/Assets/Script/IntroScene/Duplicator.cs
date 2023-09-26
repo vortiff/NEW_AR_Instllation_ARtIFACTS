cf857cf2893092ae5c6cd94c31e09ab67d527dce
vortiff <icaro.orengo@hs-furtwangen.de>
2023-09-26 12:58:05 +0200

    Setp2
    - Istallazione Immagini scultura interattiva seting up

--- ARtIFACTS/Assets/Script/IntroScene/Duplicator.cs
+++ ARtIFACTS/Assets/Script/IntroScene/Duplicator.cs
@@ -5,30 +5,71 @@ using UnityEngine;
 public class Duplicator : MonoBehaviour
 {
     public GameObject objectToDuplicate; // Il GameObject da duplicare
-    public float duplicationRadius = 5f; // Raggio in cui duplicare l'oggetto
+    public float duplicationRadius = 1f; // Raggio in cui duplicare l'oggetto
+    public float minDuplicationInterval = 2f; // Intervallo minimo tra le duplicazioni
+    public float maxDuplicationInterval = 5f; // Intervallo massimo tra le duplicazioni
 
     private Transform player; // Il trasform del giocatore
+    private bool isPlayerInsideCollider = false;
+    private float nextDuplicationTime;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform; // Trova il giocatore per tag "Player"
+        SetNextDuplicationTime();
     }
 
-    void OnCollisionEnter(Collision collision)
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        // Controlla se è il momento di duplicare
+        if (Time.time >= nextDuplicationTime && isPlayerInsideCollider)
         {
-            // Genera una posizione casuale entro il raggio specificato
-            Vector3 randomOffset = Random.insideUnitSphere * duplicationRadius;
-
-            // Calcola la posizione del nuovo oggetto
-            Vector3 newPosition = transform.position + randomOffset;
-
-            // Duplica l'oggetto
-            GameObject newObject = Instantiate(objectToDuplicate, newPosition, Quaternion.identity);
-
-            // Ruota il nuovo oggetto su tutti e tre gli assi per inseguire il giocatore
-            newObject.transform.LookAt(player);
+            DuplicateObject();
+            SetNextDuplicationTime();
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInsideCollider = true;
+            Debug.Log("Player è nel collider");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInsideCollider = false;
+            Debug.Log("Player ha lasciato il collider");
+        }
+    }
+
+    void DuplicateObject()
+    {
+        // Genera una posizione casuale entro il raggio specificato
+        Vector3 randomOffset = Random.insideUnitSphere * duplicationRadius;
+
+        // Calcola la posizione del nuovo oggetto
+        Vector3 newPosition = transform.position + randomOffset;
+
+        // Duplica l'oggetto
+        GameObject newObject = Instantiate(objectToDuplicate, newPosition, Quaternion.identity);
+
+        // Imposta il transform del nuovo oggetto come figlio del transform attuale
+        newObject.transform.SetParent(transform);
+
+        // Ruota il nuovo oggetto su tutti e tre gli assi in modo casuale
+        newObject.transform.localRotation = Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+    }
+
+
+
+    void SetNextDuplicationTime()
+    {
+        // Imposta il prossimo momento in cui verrà duplicato l'oggetto in base agli intervalli minimi e massimi
+        nextDuplicationTime = Time.time + Random.Range(minDuplicationInterval, maxDuplicationInterval);
     }
 }
