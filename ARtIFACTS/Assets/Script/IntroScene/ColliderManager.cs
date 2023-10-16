5bb9e1585899b9f6b8c3f7bdce5c0487b1c7c214
vortiff <icaro.orengo@hs-furtwangen.de>
2023-10-16 17:21:02 +0200

    SetUp Adter Crasch

--- ARtIFACTS/Assets/Script/IntroScene/ColliderManager.cs
+++ ARtIFACTS/Assets/Script/IntroScene/ColliderManager.cs
@@ -1,59 +1,48 @@
 using UnityEngine;
 using System.Collections;
 
-
 public class ColliderManager : MonoBehaviour
 {
-    
-    public GameObject metaballObject; // Il GameObject Metaball
-    public GameObject objectToActivate; // Il GameObject da attivare
-    public GameObject objectToDeactivate; // Il GameObject da disattivare
-    public GameObject nextColiderToActivate; // Il prossimo collider dell'isztallazione da attivare
-    public int audioClipIndex = 1; // L'indice dell'audio clip da riprodurre
-    private AudioClip[] metaballSounds; // Array di suoni da Metaball
+    public GameObject metaballObject;
+    public GameObject objectToActivate;
+    public GameObject objectToDeactivate;
+    public GameObject nextColiderToActivate;
+    public GameObject attractionTarget; // Aggiunto l'attractionTarget
+    public float attractionDelay = 5f; // Tempo di attesa prima che Metaball venga attratto
+    public int audioClipIndex = 1;
+    private AudioClip[] metaballSounds;
     private AudioSource audioSource;
     private bool hasCollided = false;
 
-
     private void Start()
     {
-        // Ottieni il componente AudioSource dal GameObject Metaball
         audioSource = metaballObject.GetComponent<AudioSource>();
-
-        // Ottieni l'array di clip audio dal metaballObject
         metaballSounds = metaballObject.GetComponent<Metaball>().voiceOverSounds;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Controlla se il giocatore è entrato in collisione
         if (other.CompareTag("Player") && !hasCollided)
         {
-            // Riproduci il suono VoiceOver dalla lista del GameObject Metaball
             if (audioSource != null && metaballSounds.Length > 0 && audioClipIndex < metaballSounds.Length)
             {
                 audioSource.clip = metaballSounds[audioClipIndex];
                 audioSource.Play();
             }
 
-            // Attiva il GameObject specificato
             if (objectToActivate != null)
             {
                 objectToActivate.SetActive(true);
                 nextColiderToActivate.SetActive(true);
             }
 
-            // Attiva la gravità sui cloni
             ActivateGravityOnClones();
 
-            // Avvia la coroutine per distruggere i cloni dopo 5 secondi e poi disattivare l'oggetto
-            StartCoroutine(DestroyAndDeactivate());
-            
-            // Imposta hasCollided su true per evitare collisioni multiple
+            StartCoroutine(AttractMetaballAfterDelay());
+
             hasCollided = true;
         }
-    }   
-
+    }
 
     private void ActivateGravityOnClones()
     {
@@ -65,7 +54,27 @@ public class ColliderManager : MonoBehaviour
             {
                 rb.useGravity = true;
             }
-       }
+        }
+    }
+
+    private IEnumerator AttractMetaballAfterDelay()
+    {
+        yield return new WaitForSeconds(attractionDelay);
+
+        // Fai in modo che Metaball segua l'attractionTarget
+        metaballObject.transform.LookAt(attractionTarget.transform);
+        Rigidbody metaballRb = metaballObject.GetComponent<Rigidbody>();
+        metaballRb.velocity = metaballObject.transform.forward * 5; // 5 è la velocità con cui Metaball si muove verso l'attractionTarget
+
+        // Riproduci il suono con indice 4
+        if (audioSource != null && metaballSounds.Length > 3)
+        {
+            audioSource.clip = metaballSounds[3];
+            audioSource.Play();
+        }
+
+        // Continua con la distruzione dei cloni e la disattivazione dell'oggetto
+        StartCoroutine(DestroyAndDeactivate());
     }
 
     private IEnumerator DestroyAndDeactivate()
@@ -79,7 +88,6 @@ public class ColliderManager : MonoBehaviour
         }
     }
 
-    // Funzione per distruggere i cloni con il tag "cloni"
     private void DestroyClones()
     {
         GameObject[] clones = GameObject.FindGameObjectsWithTag("cloni");
